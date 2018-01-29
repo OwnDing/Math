961e90c6dcfcaece6c99efac6374925ce47b8a5b
DingJianchen <jianchending@outlook.com>
2018-01-29 21:35:10 +0800

    Update DpShortPath.java
    路径补足

--- OperationalResearch/DpShortPath.java
+++ OperationalResearch/DpShortPath.java
@@ -9,12 +9,12 @@ public class DpShortPath {
 		map.put("c", "h,5;i,3;j,9;d,2");
 		map.put("d", "j,7;e,9");
 		map.put("e", "e,0");
-		map.put("f", "e,4;j,1");
+		map.put("f", "e,4;j,1;k,1");
 		map.put("g", "k,9;h,7");
-		map.put("h", "c,5;i,1;g,7;k,2");
+		map.put("h", "c,5;i,1;g,7;k,2;b,6");
 		map.put("i", "c,3;h,1;k,4;j,9");
-		map.put("j", "d,7;f,1;e,2");
-		map.put("k", "i,4;j,3;f,1");
+		map.put("j", "d,7;f,1;e,2;i,9;c,9;k,3");
+		map.put("k", "i,4;j,3;f,1;h,2");
 		
 		loopHashMap(map);
 	}
